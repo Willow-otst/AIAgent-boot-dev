d315de01bdb666e89d2d79ded20cd4ac0079fbe5
Willow <107652695+Willow-otst@users.noreply.github.com>
2025-06-21 23:43:56 -0500

    added verbose command

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-# Get api key from env variable
 import os
-from dotenv import load_dotenv
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-# import the genai library using the imported key
-from google import genai
-client = genai.Client(api_key=api_key)
-
-# other imports
 import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-# main
+verbose = False;
+
 if len(sys.argv) <= 1:
     print("ERROR - Please provide a prompt.")
     exit(1)
-
+for arg in sys.argv[1:]:
+    if arg in ["-v", "--verbose"]:
+        verbose = True;
+        sys.argv.remove(arg)
+        continue
 prompt = sys.argv[1]
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+messages = [types.Content(
+    role = "user",
+    parts = [types.Part(text = prompt)]),]
+
 response = client.models.generate_content(
-    model= 'gemini-2.0-flash-001',
-    contents= prompt
-)
+    model = 'gemini-2.0-flash-001',
+    contents = messages)
+
 print(response.text + "\n")
-print(
-    f"Tokens: (Prompt: {response.usage_metadata.prompt_token_count})" +
-    f"(Respnse: {response.usage_metadata.candidates_token_count})"
-)
+
+if verbose:
+    print("============================================")
+    print(f"Prompt: {prompt}\n")
+    print(
+        f"Tokens: (Prompt: {response.usage_metadata.prompt_token_count})" +
+        f"(Respnse: {response.usage_metadata.candidates_token_count})"
+    )
+    print("User prompt: Prompt tokens: Response tokens:")
